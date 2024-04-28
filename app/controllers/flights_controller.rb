28dd1d3a47a234be42d9d95d3eaf65840324fa68
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-04-28 13:32:55 +0300

    Fixed flights#index display, added #search, _form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,6 +6,27 @@ class FlightsController < ApplicationController
   end
 
   def search
+    @flights = Flight.all
+    @airports = Airport.all
+    @flight_options = @airports.map{|a| a.name_code}.uniq
+    puts params.inspect
+    
+      # Extract the airport codes from the parameters
+      departure_airport_code = params[:departure_airport].split('|').last.strip
+      arrival_airport_code = params[:arrival_airport].split('|').last.strip
+    
+      # Find the airports in the database
+      departure_airport = Airport.find_by(code: departure_airport_code)
+      arrival_airport = Airport.find_by(code: arrival_airport_code)
+    
+      # Parse the dates
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+    
+      # Find the flights
+      @flights = Flight.where(departure_airport_id: departure_airport.id, arrival_airport_id: arrival_airport.id)
+                       .where('start >= ? AND start < ?', start_date, end_date + 1.day)
+    
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'flights/index'
+  get "flights/search"
   
   root "flights#index"
 end
